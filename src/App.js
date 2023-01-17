e01ffaca9bd3cb2e073a8b3d70f68cd6b26dfac6
Ayyan <ayyanbabar11@gmail.com>
2023-01-18 00:50:54 +0500

    adding all the functionalities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>Birthday Reminder</h3>
+        <h5>{people.length} Birthdays today</h5>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
